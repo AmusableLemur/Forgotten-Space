fd09f8421d0386941270af83e72c5b73e078e821
Rasmus Larsson <rasmus@rasmuslarsson.se>
2014-03-27 12:34:17 +0100

    Game states

--- src/main/Game.java
+++ src/main/Game.java
@@ -6,9 +6,12 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
 public class Game extends BasicGame {
+	private State state;
 
 	public Game(String title) {
 		super(title);
+		
+		this.state = State.MENU;
 	}
 
 	@Override
@@ -27,6 +30,10 @@ public class Game extends BasicGame {
 	public void update(GameContainer gc, int delta) throws SlickException {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public void setState(State state) {
+		this.state = state;
 	}
 
 }
